c68ff3dc95083a34a24c2cd876c803beb2126fb9
romart007 <labb0ssfreelance@gmail.com>
2019-11-14 00:52:10 +0800

    updates for hex and slider sections

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,12 @@
 
 import 'bootstrap';
 import '../css/style.scss';
+
+$('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    centerMode: true,
+    dots: true,
+    speed: 500
+});
