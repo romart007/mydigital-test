350a3fb2e5d1c217b97dd84d95d277f8d026259c
romart007 <labb0ssfreelance@gmail.com>
2019-11-17 13:40:50 +0800

    added dots and login section

--- src/js/app.js
+++ src/js/app.js
@@ -11,3 +11,20 @@ $('.multiple-items').slick({
     dots: true,
     speed: 500
 });
+
+
+//dynacmic dots 
+const imgBuilding = document.querySelector('.building').offsetWidth / 2;
+const imgBuilding2 = document.querySelector('.building2').offsetWidth / 2;
+const img1 = document.querySelector('.dots-1');
+
+function setDots() {
+    img1.style.right = `${imgBuilding}px`;
+    img1.style.left = `${imgBuilding2}px`;
+    const temp = imgBuilding + imgBuilding;
+
+    img1.style.width = `calc(100% - ${temp}px)`;
+}
+
+window.addEventListener('load', setDots);
+window.onresize = setDots;
